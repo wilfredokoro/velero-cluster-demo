538992539f0164d2732852bd60aa2210f499547b
wilfredokoro <101127016+wilfredokoro@users.noreply.github.com>
2024-10-28 21:08:52 -0500

    coded

--- eks.tf
+++ eks.tf
@@ -31,9 +31,9 @@ module "eks" {
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["t3.medium"]
 
-      min_size     = 2
-      max_size     = 5
-      desired_size = 2
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
 
 
     }
